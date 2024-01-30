84acae2128ccfa9276efddd80e1e38475af3d92c
4abishai <146672653+4abishai@users.noreply.github.com>
2024-01-30 16:53:03 +0530

    Answer of Ques A

---
+++ 30.01.24/A.java
@@ -0,0 +1,55 @@
+// A plastic manufacturer sells plastic in different shapes like 2D sheet and
+// 3D box. The cost of a sheet is Rs 40/- per square ft. and the cost of a box 
+// is Rs 60/- per cubic ft. Implement it in Java to calculate the cost of 
+// plastic as per the dimensions given by the user where 3D inherits from 2D.
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        sheet2D sheet = new sheet2D();
+        box3D box = new box3D();
+
+        System.out.print("Enter height: ");
+        sheet.length = box.length = scan.nextFloat();
+
+        System.out.print("Enter breadth: ");
+        sheet.breadth = box.breadth = scan.nextFloat();
+
+        System.out.print("Enter height: ");
+        box.height = scan.nextFloat();
+
+        System.out.println("Cost of 2D sheet: " + sheet.getCost());
+        System.out.print("Cost of 3D box: " + box.getCost());
+
+        scan.close();
+
+    }
+}
+
+class sheet2D {
+    float length, breadth;
+    static int cost = 40;
+
+    float getArea() {
+        return (length * breadth);
+    }
+
+    float getCost() {
+        return (getArea() * cost);
+    }
+}
+
+class box3D extends sheet2D {
+    float height;
+    static int cost = 60;
+
+    float getArea() {
+        return (length * breadth * height);
+    }
+
+    float getCost() {
+        return (this.getArea() * cost);
+    }
+}
